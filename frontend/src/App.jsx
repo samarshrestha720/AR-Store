973eb732b64db4959298bc48db0e56a194a32b88
umesh625 <umeshshrestha903@gmail.com>
2024-05-16 14:36:18 +0545

    2:35(30:21)updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
 
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import ShopCategory from './Pages/ShopCategory'
+import Product from './Pages/Product'
+import LoginSignup from './Pages/LoginSignup'
 
 function App() {
   return (
    
       <div >
-      <Navbar/>
+        <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/mens' element={<ShopCategory category="mens" />}/>
+          <Route path='/womens' element={<ShopCategory category="women" />}/>
+          <Route path='/kids' element={<ShopCategory category="kid" />}/>
+          <Route path="/product" element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+            </Route>
+            <Route path='/cart' element={<Cart/>}/>
+            <Route path='/login' element={<LoginSignup/>}/>
+
+        </Routes>
+      </BrowserRouter>
       </div>
      
     
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assests/logo.png'
 import cart_icon from '../Assests/cart_icon.png'
+import { Link } from 'react-router-dom'
 
  const Navbar= () => {
+     const[menu,setMenu]=useState("shop");
   return (
     <div className='navbar'>
      <div className="nav-logo">
           <img src={logo} alt="" />
-          <p>SHOPPER</p>
+          <p>AR SHOP.</p>
      </div>
      <ul className="nav-menu">
-          <li>Shop</li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>Kids</li>
+          <li onClick={()=>{setMenu("shop")}}><Link to ={'/'}>Shop</Link>{menu === "shop" ? <hr /> : <></>}</li>
+          <li onClick={()=>{setMenu("mens")}}><Link to ={'/mens'}>Men</Link>{menu === "mens" ? <hr /> : <></>}</li>
+          <li onClick={()=>{setMenu("womens")}}><Link to ={'/womens'}>Women</Link>{menu === "womens" ? <hr /> : <></>}</li>
+          <li onClick={()=>{setMenu("kids")}}><Link to ={'/kids'}>Kids</Link>{menu === "kids" ? <hr /> : <></>}</li>
      </ul>
      <div className="nav-login-cart">
-          <button>Login</button>
-          <img src={cart_icon} alt="" />
+          <Link to='/login'><button>Login</button></Link>
+          <Link to='/cart'> <img src={cart_icon} alt="" /></Link>
+         
+<div className="nav-cart-count">0</div>
 
      </div>
 
