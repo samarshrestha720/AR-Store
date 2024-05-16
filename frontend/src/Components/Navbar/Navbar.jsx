ed93266dba4fafb3f17b3a5f411b51c2bd121423
umesh625 <umeshshrestha903@gmail.com>
2024-05-16 12:33:07 +0545

    12:32 (5/16) updated

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../Assests/logo.png'
+import cart_icon from '../Assests/cart_icon.png'
+
+ const Navbar= () => {
+  return (
+    <div className='navbar'>
+     <div className="nav-logo">
+          <img src={logo} alt="" />
+          <p>SHOPPER</p>
+     </div>
+     <ul className="nav-menu">
+          <li>Shop</li>
+          <li>Men</li>
+          <li>Women</li>
+          <li>Kids</li>
+     </ul>
+     <div className="nav-login-cart">
+          <button>Login</button>
+          <img src={cart_icon} alt="" />
+
+     </div>
+
+    </div>
+  )
+}
+export default Navbar
